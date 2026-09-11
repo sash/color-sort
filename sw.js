79df0519da02909dec8900b5908b582548e1e88f
SASh <sash@users.noreply.github.com>
2026-09-12 01:27:01 +0300

    Show a full horizontal rainbow in every wildcard layer and pour

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD = '23ec4d0e1e28';
+const BUILD = '5467e01ec1c5';
 const CACHE = `color-sort-${BUILD}`;
 const ASSETS = ['index.html', 'manifest.webmanifest', 'levels.json', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', event => {
