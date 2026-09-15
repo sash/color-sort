208d4dd5547760a19fd1108efb664178c708d251
SASh <sash@users.noreply.github.com>
2026-09-15 16:04:08 +0300

    Leave surplus and large bottle color deductions to the player

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD = 'ff6f8a60abd1';
+const BUILD = '2968096f4184';
 const CACHE = `color-sort-${BUILD}`;
 const ASSETS = ['index.html', 'manifest.webmanifest', 'levels.json', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', event => {
