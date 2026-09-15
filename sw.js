6f6d4d690046206cecbad39618e4f87180fce031
SASh <sash@users.noreply.github.com>
2026-09-15 15:56:57 +0300

    Make heavy liquid visibly weighted and animate its descent

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD = '32d8f935db1f';
+const BUILD = 'ff6f8a60abd1';
 const CACHE = `color-sort-${BUILD}`;
 const ASSETS = ['index.html', 'manifest.webmanifest', 'levels.json', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', event => {
